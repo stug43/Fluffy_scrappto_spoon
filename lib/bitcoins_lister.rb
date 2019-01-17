82887445092e09705acd2d65215608d63904eaf0
stug43 <nganta.a@hotmail.fr>
2019-01-17 23:40:47 +0100

    il est 23h 40 j'ai le seum

--- lib/bitcoins_lister.rb
+++ lib/bitcoins_lister.rb
@@ -1,13 +1,16 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
-
+def perform
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 
 names = page.css("//td[@class='text-left col-symbol']/text()").to_a
 values = page.css("//*[@class='price']/@data-usd").to_a
 final_array = [names.map! {|k| k.text}, values.map!{|k| k.text}].transpose.map! {|k| [k].to_h}
-puts final_array
+return final_array
+end
+
+puts perform
 
 file = File.open("bitcoins.txt","w+")
 fichier = file
---
+++ spec/bitcoins_lister_spec.rb
@@ -0,0 +1,8 @@
+require_relative ".../lib/bitcoins_lister.rb"
+
+describe "the perform method" do
+	it "should return an array of hashs" do
+expect(perform().size>50).to eq(true)
+expect(perform().keys[7].class).to eq(String)
+end
+end
